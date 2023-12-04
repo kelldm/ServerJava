16c978bb565d6bab5f0019acc879aaf34f3b1147
kelldm <raquellinsart@gmail.com>
2023-12-04 12:01:02 -0300

    add

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,59 @@
-package org.example;public class Main {
+package org.example;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+
+import static spark.Spark.*;
+
+public class Main {
+    private static int totalRequis;
+
+    private static String nomeRequis;
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        JFrame frame = new JFrame("Servidor");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600,400);
+        frame.setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(4,2));
+
+        JTextField inputField1 = new JTextField();
+        JTextField inputField2 = new JTextField();
+
+        inputField1.setFont((new Font("Arial", Font.PLAIN, 30)));
+        inputField2.setFont((new Font("Arial", Font.PLAIN, 30)));
+
+
+        JLabel label1 = new JLabel("Nome:");
+        label1.setFont((new Font("Arial", Font.BOLD, 30)));
+        JLabel label2 = new JLabel("Acesso:");
+        label2.setFont((new Font("Arial", Font.BOLD, 30)));
+
+
+        panel.add(label1);
+        panel.add(inputField1);
+        panel.add(label2);
+        panel.add(inputField2);
+
+
+        frame.add(panel);
+        frame.setVisible(true);
+
+            port(8080);
+            get("/api/:nome", (req, res)->{
+                String nome = req.params(":nome");
+                nomeRequis = nome;
+           totalRequis++;
+           inputField1.setText(nomeRequis);
+           inputField2.setText(Integer.toString(totalRequis));
+           return String.valueOf(totalRequis);
+            });
     }
 }
+
